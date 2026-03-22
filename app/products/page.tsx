df641bc3f6e842ad040972d8d686b1933689008f
ramadanmariam330 <ramadanmariam330@gmail.com>
2026-03-22 17:48:00 +0200

    Add dynamic export to products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = 'force-dynamic';
 import { useState, useMemo } from "react";
 import { useSearchParams } from "next/navigation";
 import { Navbar } from "@/components/navbar";
